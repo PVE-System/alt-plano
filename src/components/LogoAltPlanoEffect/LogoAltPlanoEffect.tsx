6171b1123b573646574a9827742fe46db26d2167
Caio Espindola <caioespindola@hotmail.com>
2025-06-30 17:10:52 -0300

    fix bug logo mobile

--- src/components/LogoAltPlanoEffect/LogoAltPlanoEffect.tsx
+++ src/components/LogoAltPlanoEffect/LogoAltPlanoEffect.tsx
@@ -9,11 +9,12 @@ export default function LogoAltPlanoEffect() {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        overflow: 'hidden',
       }}
     >
       <Box
         component="img"
-        src="/images/AltPlano_logomarca_ok.svg"
+        src="/images/AltPlano_logomarca2.svg"
         alt="Alt Plano Logo"
         sx={{
           width: { xs: '200px', sm: '200px', md: '280px' },
